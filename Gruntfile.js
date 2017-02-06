6607aa92e1f1823eee9aaa9936fc80d8844b422d
Torkel Ödegaard <torkel@grafana.org>
2017-02-06 17:57:20 +0100

    removed grunt-execute

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,6 @@ module.exports = function(grunt) {
 
   require("load-grunt-tasks")(grunt);
 
-  grunt.loadNpmTasks("grunt-execute");
   grunt.loadNpmTasks("grunt-contrib-clean");
 
   var env = grunt.option('env') || "dev";
